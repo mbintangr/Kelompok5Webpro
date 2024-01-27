18a5ba6d8980becebef2ea43ac16ff23f17a5319
Yzerhaf04 <teuku.rafli.2104@gmail.com>
2024-01-27 23:11:10 +0700

    ke 1

---
+++ Daftar Ruangan/drDosen.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Container Button</title>
+    <!-- Tambahkan link ke Tailwind CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <div class="bg-blue-500 text-white font-bold py-5 px-5 mb-10">
+        Gedung AA
+    </div>
+
+    <div class="flex justify-center items-center h-auto ">
+        <div class="grid grid-cols-3 gap-4">
+            <button type="submit" class="container bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4  transition duration-300">
+                <div>click</div>
+                <div>AA 202</div>
+                <div>Kapasitas 30</div>
+            </button>
+            <button type="submit" class="container bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4  transition duration-300">
+                <div>click</div>
+                <div>AA 202</div>
+                <div>Kapasitas 30</div>
+            </button>
+            <button type="submit" class="container bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4  transition duration-300">
+                <div>click</div>
+                <div>AA 202</div>
+                <div>Kapasitas 30</div>
+            </button>
+            <!-- Baris kedua -->
+            <button type="submit" class="container bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4  transition duration-300">
+                <div>click</div>
+                <div>AA 202</div>
+                <div>Kapasitas 30</div>
+            </button>
+            <button type="submit" class="container bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4  transition duration-300">
+                <div>click</div>
+                <div>AA 202</div>
+                <div>Kapasitas 30</div>
+            </button>
+            <button type="submit" class="container bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4  transition duration-300">
+                <div>click</div>
+                <div>AA 202</div>
+                <div>Kapasitas 30</div>
+            </button>
+            <!-- Baris ketiga -->
+            <button type="submit" class="container bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4  transition duration-300">
+                <div>click</div>
+                <div>AA 202</div>
+                <div>Kapasitas 30</div>
+            </button>
+            <button type="submit" class="container bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4  transition duration-300">
+                <div>click</div>
+                <div>AA 202</div>
+                <div>Kapasitas 30</div>
+            </button>
+            <button type="submit" class="container bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4  transition duration-300">
+                <div>click</div>
+                <div>AA 202</div>
+                <div>Kapasitas 30</div>
+            </button>
+        </div>
+    </div>
+</body>
+
+</html>
