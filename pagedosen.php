7df5428c24ab694710afc6036639c772fbc9c507
GanzR13 <rizkimubin13@gmail.com>
2024-01-27 23:54:34 +0700

    pagedosen

--- pagedosen.php
+++ pagedosen.php
@@ -4,7 +4,6 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Page Dosen</title>
-<<<<<<< HEAD
   <link href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css" rel="stylesheet">
   <style>
     body {
