223630019147cdbcba10f8c9c4b394db2fa41eaf
Bintang Ramadhan <mbintangr13@gmail.com>
2024-01-27 21:38:30 +0700

    database setup

--- koneksi.php
+++ koneksi.php
@@ -1,13 +1,11 @@
 <?php
-$servername = "103.167.35.113";
-$username = "delatte";
-$password = "Everettt707123";
+    $db_hostname = "103.167.35.113";
+    $db_username = "delatte";
+    $db_password = "Everettt707123";
+    $db_name = "webprobanting";
+    $conn = mysqli_connect($db_hostname, $db_username, $db_password, $db_name);
 
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=webprobanting", $username, $password);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    echo "Koneksi gagal: " . $e->getMessage();
-}
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
 ?>
