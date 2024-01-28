d937831a334bdf64f91042c609b27b6b36b373e6
Bintang Ramadhan <mbintangr13@gmail.com>
2024-01-29 00:24:02 +0700

    edit styles

--- pageadmin.php
+++ pageadmin.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,54 +12,53 @@
       margin: 2cm;
     }
 
-    header{
+    header {
       margin-bottom: 1cm;
     }
 
     table {
       border-collapse: collapse;
-      width: 100%;
       margin-bottom: 1cm;
     }
 
-    th, td {
+    th,
+    td {
       border: 1px solid #000;
-      padding: 8px;
-      text-align: left;
+      padding: 10px 30px 10px 30px;
+      text-align: center;
     }
 
     th {
       background-color: #f2f2f2;
     }
-
   </style>
 </head>
 
 <body>
   <?php include_once('koneksi.php') ?>
-  <header>
-    <?php 
-      $nama_ruangan = strtoupper($_GET['ruangan']);
-      $sql = "SELECT kapasitas, jenis_ruangan FROM ruangan WHERE nama_ruangan = '$nama_ruangan'";
-      $stmt = $conn->prepare($sql);
-      $stmt->execute();
+  <header class="text-center">
+    <?php
+    $nama_ruangan = strtoupper($_GET['ruangan']);
+    $sql = "SELECT kapasitas, jenis_ruangan FROM ruangan WHERE nama_ruangan = '$nama_ruangan'";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
 
-      $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-      $kapasitas_kelas = $result[0]['kapasitas'];
-      $jenis_ruangan = $result[0]['jenis_ruangan'];
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $kapasitas_kelas = $result[0]['kapasitas'];
+    $jenis_ruangan = $result[0]['jenis_ruangan'];
     ?>
-    <h2 class='text-2xl font-bold'><b>RUANG <?php echo $nama_ruangan;?></b></h2>
-    <p>Jenis Ruangan: <?php echo $jenis_ruangan ;?></p>
-    <p>Kapasitas: <?php echo $kapasitas_kelas . ' orang';?></p>
+    <h2 class='text-2xl font-bold'><b>RUANG <?php echo $nama_ruangan; ?></b></h2>
+    <p>Jenis Ruangan: <?php echo $jenis_ruangan; ?></p>
+    <p>Kapasitas: <?php echo $kapasitas_kelas . ' orang'; ?></p>
   </header>
 
   <main>
-  <table class="min-w-full mb-5">
-  <h1 class="mb-2"><b>JADWAL</b></h1>
+    <table class="mb-5 mx-auto">
+      <h1 class="mb-6 text-center text-xl"><b>JADWAL</b></h1>
       <thead>
         <tr>
           <th>Nama Dosen</th>
-          <th>Matkul</th>
+          <th>Mata Kuliah</th>
           <th>Semester</th>
           <th>Kelas</th>
           <th>Waktu</th>
@@ -67,35 +67,35 @@
         </tr>
       </thead>
       <tbody>
-        <?php           
-          $sql = "SELECT u.nama, mk.nama_mata_kuliah, mk.semester, pr.kelas, pr.waktu, pr.hari, pr.status FROM peminjaman_ruangan pr JOIN user u ON (pr.id_user = u.id) JOIN mata_kuliah mk ON (mk.id_user = u.id) WHERE id_ruangan = (SELECT id_ruangan FROM ruangan WHERE nama_ruangan = '$nama_ruangan')";
-          $stmt = $conn->prepare($sql);
-          $stmt->execute();
-        
-          $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-          foreach ($stmt->fetchAll() as $key => $value) {
-            echo "<tr>";
-              echo "<td>" . $value['nama'] . "</td>";
-              echo "<td>" . $value['nama_mata_kuliah'] . "</td>";
-              echo "<td>" . $value['semester'] . "</td>";
-              echo "<td>" . $value['kelas'] . "</td>";
-              echo "<td>" . $value['waktu'] . "</td>";
-              echo "<td>" . $value['hari'] . "</td>";
-              if ($value['status'] == 'Accepted') {
-                echo "<td><div class='flex justify-items-start'><button class='px-5 py-1 bg-green-500 font-bold text-white rounded-full'>" . $value['status'] . "</button>";
-                echo "<a href='editstatus.php?waktu=" . $value['waktu'] . "&hari=" . $value['hari'] . "&ruangan=" . $nama_ruangan . "'><button class='ml-2 px-5 py-1 bg-black font-bold text-white rounded-full'>Edit</button>";
-                echo "<a href='hapusdata.php?waktu=" . $value['waktu'] . "&hari=" . $value['hari'] . "&ruangan=" . $nama_ruangan . "'><button class='ml-2 px-5 py-1 bg-red-700 font-bold text-white rounded-full'>X</button></div></td>";
-              } else if ($value['status'] == 'Pending') {
-                echo "<td><div class='flex justify-items-start'><button class='px-5 py-1 bg-yellow-500 font-bold text-white rounded-full'>" . $value['status'] . "</button>";
-                echo "<a href='editstatus.php?waktu=" . $value['waktu'] . "&hari=" . $value['hari'] . "&ruangan=" . $nama_ruangan . "'><button class='ml-2 px-5 py-1 bg-black font-bold text-white rounded-full'>Edit</button>";
-                echo "<a href='hapusdata.php?waktu=" . $value['waktu'] . "&hari=" . $value['hari'] . "&ruangan=" . $nama_ruangan . "'><button class='ml-2 px-5 py-1 bg-red-700 font-bold text-white rounded-full'>X</button></div></td>";
-              } else {
-                echo "<td><div class='flex justify-items-start'><button class='px-5 py-1 bg-red-500 font-bold text-white rounded-full'>" . $value['status'] . "</button>";
-                echo "<a href='editstatus.php?waktu=" . $value['waktu'] . "&hari=" . $value['hari'] . "&ruangan=" . $nama_ruangan . "'><button class='ml-2 px-5 py-1 bg-black font-bold text-white rounded-full'>Edit</button>";
-                echo "<a href='hapusdata.php?waktu=" . $value['waktu'] . "&hari=" . $value['hari'] . "&ruangan=" . $nama_ruangan . "'><button class='ml-2 px-5 py-1 bg-red-700 font-bold text-white rounded-full'>X</button></div></td>";
-              }
-            echo "</tr>";
+        <?php
+        $sql = "SELECT u.nama, mk.nama_mata_kuliah, mk.semester, pr.kelas, pr.waktu, pr.hari, pr.status FROM peminjaman_ruangan pr JOIN user u ON (pr.id_user = u.id) JOIN mata_kuliah mk ON (mk.id_user = u.id) WHERE id_ruangan = (SELECT id_ruangan FROM ruangan WHERE nama_ruangan = '$nama_ruangan')";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+
+        $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        foreach ($stmt->fetchAll() as $key => $value) {
+          echo "<tr>";
+          echo "<td>" . $value['nama'] . "</td>";
+          echo "<td>" . $value['nama_mata_kuliah'] . "</td>";
+          echo "<td>" . $value['semester'] . "</td>";
+          echo "<td>" . $value['kelas'] . "</td>";
+          echo "<td>" . $value['waktu'] . "</td>";
+          echo "<td>" . $value['hari'] . "</td>";
+          if ($value['status'] == 'Accepted') {
+            echo "<td><div class='flex justify-items-start'><button class='px-5 py-1 bg-green-500 font-bold text-white rounded-full'>" . $value['status'] . "</button>";
+            echo "<a href='editstatus.php?waktu=" . $value['waktu'] . "&hari=" . $value['hari'] . "&ruangan=" . $nama_ruangan . "'><button class='ml-2 px-5 py-1 bg-black font-bold text-white rounded-full'>Edit</button>";
+            echo "<a href='hapusdata.php?waktu=" . $value['waktu'] . "&hari=" . $value['hari'] . "&ruangan=" . $nama_ruangan . "'><button class='ml-2 px-5 py-1 bg-red-700 font-bold text-white rounded-full'>X</button></div></td>";
+          } else if ($value['status'] == 'Pending') {
+            echo "<td><div class='flex justify-items-start'><button class='px-5 py-1 bg-yellow-500 font-bold text-white rounded-full'>" . $value['status'] . "</button>";
+            echo "<a href='editstatus.php?waktu=" . $value['waktu'] . "&hari=" . $value['hari'] . "&ruangan=" . $nama_ruangan . "'><button class='ml-2 px-5 py-1 bg-black font-bold text-white rounded-full'>Edit</button>";
+            echo "<a href='hapusdata.php?waktu=" . $value['waktu'] . "&hari=" . $value['hari'] . "&ruangan=" . $nama_ruangan . "'><button class='ml-2 px-5 py-1 bg-red-700 font-bold text-white rounded-full'>X</button></div></td>";
+          } else {
+            echo "<td><div class='flex justify-items-start'><button class='px-5 py-1 bg-red-500 font-bold text-white rounded-full'>" . $value['status'] . "</button>";
+            echo "<a href='editstatus.php?waktu=" . $value['waktu'] . "&hari=" . $value['hari'] . "&ruangan=" . $nama_ruangan . "'><button class='ml-2 px-5 py-1 bg-black font-bold text-white rounded-full'>Edit</button>";
+            echo "<a href='hapusdata.php?waktu=" . $value['waktu'] . "&hari=" . $value['hari'] . "&ruangan=" . $nama_ruangan . "'><button class='ml-2 px-5 py-1 bg-red-700 font-bold text-white rounded-full'>X</button></div></td>";
           }
+          echo "</tr>";
+        }
         ?>
       </tbody>
     </table>
@@ -104,7 +104,7 @@
       <div class='fixed top-0 left-0 w-full h-screen bg-black opacity-50'>
       </div>
       <div class='flex justify-center items-center fixed bg-white rounded-3xl p-8 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
-        <a href="/" class='absolute top-3 right-5 font-extrabold' onclick=<?php $isPopup = false?>>X</a>
+        <a href="/" class='absolute top-3 right-5 font-extrabold' onclick=<?php $isPopup = false ?>>X</a>
         <div>
           <h2 class='font-bold text-center mb-3'>Change Status:</h2>
           <div>
@@ -118,4 +118,5 @@
 
   </main>
 </body>
+
 </html>
--- pagedosen.php
+++ pagedosen.php
@@ -9,6 +9,7 @@ $user = $statement->fetch(PDO::FETCH_ASSOC);
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -21,74 +22,70 @@ $user = $statement->fetch(PDO::FETCH_ASSOC);
       margin-right: 2cm;
     }
 
-    header{
+    header {
       margin-bottom: 1cm;
     }
 
     table {
       border-collapse: collapse;
-      width: 100%;
     }
 
-    th, td {
+    th,
+    td {
       border: 1px solid #000;
-      padding: 8px;
-      text-align: left;
+      padding: 10px 30px 10px 30px;
+      text-align: center;
     }
 
     th {
       background-color: #f2f2f2;
     }
-
-
   </style>
 </head>
 
 <body>
-
-  
-  <header>
-  <nav class=" mb-10 bg-white border-gray-200 dark:bg-gray-900 relative ">
-    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+  <header class="text-center">
+    <nav class=" mb-10 bg-white border-gray-200 dark:bg-gray-900 relative ">
+      <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="index.php" class="flex items-center space-x-3 rtl:space-x-reverse">
-            <img src="img/pnjlogo.png" class="h-12" />
-            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">POLITEKNIK NEGERI JAKARTA</span>
+          <img src="img/pnjlogo.png" class="h-12" />
+          <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">POLITEKNIK NEGERI JAKARTA</span>
         </a>
         <div class="flex items-center md:order-2 space-x-1 md:space-x-0 rtl:space-x-reverse">
-        <button type="button" data-dropdown-toggle="language-dropdown-menu" onclick="toggleDropdown()" class="inline-flex items-center font-medium justify-center px-4 py-2 text-sm text-gray-900 dark:text-white rounded-lg cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white dropdown-toggle">
-        <div class="w-12 h-12">
-        <img src="img/user.png"/>
+          <button type="button" data-dropdown-toggle="language-dropdown-menu" onclick="toggleDropdown()" class="inline-flex items-center font-medium justify-center px-4 py-2 text-sm text-gray-900 dark:text-white rounded-lg cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white dropdown-toggle">
+            <div class="w-12 h-12">
+              <img src="img/user.png" />
+            </div>
+            <p>Welcome, <?php echo $user['nama']; ?></p>
+          </button>
+          <button type="button" class="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 mt-2 ml-8 text-center me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"><a href="logout.php">Logout</a></button>
         </div>
-        <p>Welcome, <?php echo $user['nama']; ?></p>
-        </button>
-        <button type="button" class="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 mt-2 ml-8 text-center me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"><a href="logout.php">Logout</a></button>
-        </div>
-    </div>
-</nav>
+      </div>
+    </nav>
 
-  
-    <?php 
-      $nama_ruangan = strtoupper($_GET['ruangan']);
-      $sql = "SELECT kapasitas, jenis_ruangan FROM ruangan WHERE nama_ruangan = '$nama_ruangan'";
-      $stmt = $conn->prepare($sql);
-      $stmt->execute();
 
-      $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-      $kapasitas_kelas = $result[0]['kapasitas'];
-      $jenis_ruangan = $result[0]['jenis_ruangan'];
+    <?php
+    $nama_ruangan = strtoupper($_GET['ruangan']);
+    $sql = "SELECT kapasitas, jenis_ruangan FROM ruangan WHERE nama_ruangan = '$nama_ruangan'";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $kapasitas_kelas = $result[0]['kapasitas'];
+    $jenis_ruangan = $result[0]['jenis_ruangan'];
     ?>
-    <h2 class='text-2xl font-bold'><b>RUANG <?php echo $nama_ruangan;?></b></h2>
-    <p>Jenis Ruangan: <?php echo $jenis_ruangan ;?></p>
-    <p>Kapasitas: <?php echo $kapasitas_kelas . ' orang';?></p>
+    <h2 class='text-2xl font-bold'><b>RUANG <?php echo $nama_ruangan; ?></b></h2>
+    <p>Jenis Ruangan: <?php echo $jenis_ruangan; ?></p>
+    <p>Kapasitas: <?php echo $kapasitas_kelas . ' orang'; ?></p>
   </header>
 
   <main>
-    <table class="min-w-full mb-5">
-    <h1 class="mb-2"><b>JADWAL</b></h1>
+    <table class="mb-5 mx-auto">
+      <h1 class="mb-6 text-center text-xl"><b>JADWAL</b></h1>
       <thead>
         <tr>
           <th>Nama Dosen</th>
-          <th>Matkul</th>
+          <th>Mata Kuliah</th>
           <th>Semester</th>
           <th>Kelas</th>
           <th>Waktu</th>
@@ -97,33 +94,34 @@ $user = $statement->fetch(PDO::FETCH_ASSOC);
         </tr>
       </thead>
       <tbody>
-        <?php 
-          $sql = "SELECT u.nama, mk.nama_mata_kuliah, mk.semester, pr.kelas, pr.waktu, pr.hari, pr.status FROM peminjaman_ruangan pr JOIN user u ON (pr.id_user = u.id) JOIN mata_kuliah mk ON (mk.id_user = u.id) WHERE id_ruangan = (SELECT id_ruangan FROM ruangan WHERE nama_ruangan = '$nama_ruangan')";
-          $stmt = $conn->prepare($sql);
-          $stmt->execute();
-        
-          $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-          foreach ($stmt->fetchAll() as $key => $value) {
-            echo "<tr>";
-              echo "<td>" . $value['nama'] . "</td>";
-              echo "<td>" . $value['nama_mata_kuliah'] . "</td>";
-              echo "<td>" . $value['semester'] . "</td>";
-              echo "<td>" . $value['kelas'] . "</td>";
-              echo "<td>" . $value['waktu'] . "</td>";
-              echo "<td>" . $value['hari'] . "</td>";
-              if ($value['status'] == 'Accepted') {
-                echo "<td class='text-green-500 font-bold'>" . $value['status'] . "</td>";
-              } else if ($value['status'] == 'Pending') {
-                echo "<td class='text-yellow-500 font-bold'>" . $value['status'] . "</td>";
-              } else {
-                echo "<td class='text-red-500 font-bold'>" . $value['status'] . "</td>";
-              }
-            echo "</tr>";
+        <?php
+        $sql = "SELECT u.nama, mk.nama_mata_kuliah, mk.semester, pr.kelas, pr.waktu, pr.hari, pr.status FROM peminjaman_ruangan pr JOIN user u ON (pr.id_user = u.id) JOIN mata_kuliah mk ON (mk.id_user = u.id) WHERE id_ruangan = (SELECT id_ruangan FROM ruangan WHERE nama_ruangan = '$nama_ruangan')";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+
+        $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        foreach ($stmt->fetchAll() as $key => $value) {
+          echo "<tr>";
+          echo "<td>" . $value['nama'] . "</td>";
+          echo "<td>" . $value['nama_mata_kuliah'] . "</td>";
+          echo "<td>" . $value['semester'] . "</td>";
+          echo "<td>" . $value['kelas'] . "</td>";
+          echo "<td>" . $value['waktu'] . "</td>";
+          echo "<td>" . $value['hari'] . "</td>";
+          if ($value['status'] == 'Accepted') {
+            echo "<td class='text-green-500 font-bold'>" . $value['status'] . "</td>";
+          } else if ($value['status'] == 'Pending') {
+            echo "<td class='text-yellow-500 font-bold'>" . $value['status'] . "</td>";
+          } else {
+            echo "<td class='text-red-500 font-bold'>" . $value['status'] . "</td>";
           }
+          echo "</tr>";
+        }
         ?>
       </tbody>
     </table>
-    <a href="form.php?ruangan=<?php echo $nama_ruangan; ?>" ><button class="font-bold px-5 py-1 rounded-full bg-green-600 hover:bg-green-700 transition duration-300 mt-6 text-white">Tambah</button></a>
+    <a href="form.php?ruangan=<?php echo $nama_ruangan; ?>"><button class="font-bold px-5 py-1 rounded-full bg-green-600 hover:bg-green-700 transition duration-300 mt-6 text-white">Tambah</button></a>
   </main>
 </body>
+
 </html>
