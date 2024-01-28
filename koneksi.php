e36fb80541434ef79156d11165d2293c7545d9a3
Akmaleee <masakmaltop@Gmail.com>
2024-01-28 11:57:01 +0700

    update koneksi akmal

--- koneksi.php
+++ koneksi.php
@@ -1,11 +1,13 @@
 <?php
-    $db_hostname = "103.167.35.113";
-    $db_username = "delatte";
-    $db_password = "Everettt707123";
-    $db_name = "webprobanting";
-    $conn = mysqli_connect($db_hostname, $db_username, $db_password, $db_name);
+$servername = "103.167.35.113";
+$username = "delatte";
+$password = "Everettt707123";
 
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=webprobanting", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Koneksi gagal: " . $e->getMessage();
+}
 ?>
