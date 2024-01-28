07ae4cfc88e56847ccb7849b27f47dd276275759
Bintang Ramadhan <mbintangr13@gmail.com>
2024-01-28 22:04:36 +0700

    add homepage

--- dosen.php
+++ dosen.php
@@ -1,26 +1,98 @@
 <?php
-
 session_start();
+include("koneksi.php");
+
+
+// Fetch data from the database
+$query = "SELECT * FROM ruangan";
+$statement = $conn->prepare($query);
+$statement->execute();
+$results = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+$query = "SELECT * FROM user WHERE username = :username";
+$statement = $conn->prepare($query);
+$statement->execute(array('username' => $_SESSION["username"]));
+$user = $statement->fetch(PDO::FETCH_ASSOC);
 
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin side</title>
+    <title>List Kelas</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css">
 </head>
 <body>
-    
-<b>Halaman Dosen<b>
 
-<a href = "logout.php">Log out</a>
+<nav class="bg-white border-gray-200 dark:bg-gray-900 relative">
+    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <a href="index.php" class="flex items-center space-x-3 rtl:space-x-reverse">
+            <img src="img/pnjlogo.png" class="h-8" />
+            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">POLITEKNIK NEGERI JAKARTA</span>
+        </a>
+        <div class="flex items-center md:order-2 space-x-1 md:space-x-0 rtl:space-x-reverse">
+        <button type="button" data-dropdown-toggle="language-dropdown-menu" onclick="toggleDropdown()" class="inline-flex items-center font-medium justify-center px-4 py-2 text-sm text-gray-900 dark:text-white rounded-lg cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white dropdown-toggle">
+        <div class="w-12 h-12">
+        <img src="img/user.png"/>
+        </div>
+        <p>Welcome, <?php echo $user['nama']; ?></p>
+    </button>
+            <!-- Dropdown -->
+            <div class=" hidden my-[1000px] mx-[30px] text-base list-none bg-white divide-gray-100 rounded-lg shadow dark:bg-gray-700 absolute " id="language-dropdown-menu">
+                <ul class="py-2 font-medium" role="none" >
+                    <!-- Tambahkan opsi untuk Logout di sini -->
+                    <li >
+                        <a href="logout.php" class="block px-4 py-1 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">
+                            <div class="inline-flex items-center">              
+                                Logout
+                            </div>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</nav>
+
+</div>
+    <div class="flex justify-center items-center pb-[100px]">
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 p-4">
+            <?php foreach ($results as $result): ?>
+                <div class="bg-gray-100 p-4 rounded-md shadow-md">
+                    <h2 class="font-bold text-lg mb-4 pl-14 ;"><?php echo $result['nama_ruangan']; ?></h2>
+                    <p class="text-sm text-gray-600">Jenis Ruangan: <?php echo $result['jenis_ruangan']; ?></p>
+                    <p class="text-sm text-gray-600">Kapasitas: <?php echo $result['kapasitas']; ?></p>
+                    <button class="bg-blue-600 text-white font-bold px-4 py-1 rounded-md my-2 hover:bg-blue-700 transition duration-300 mt-6">
+                        <a href="#" target="_blank">More</a>
+                    </button>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    </div>
 
 
 
+    <script>
+    // Fungsi untuk menangani perubahan status dropdown
+    function toggleDropdown() {
+        var dropdownMenu = document.getElementById('language-dropdown-menu');
+        dropdownMenu.classList.toggle('hidden'); // Toggle class 'hidden'
+    }
+
+    // Fungsi untuk menutup dropdown saat mengklik di luar dropdown
+    window.onclick = function(event) {
+        var dropdownMenu = document.getElementById('language-dropdown-menu');
+        if (!event.target.matches('.dropdown-toggle')) {
+            // Jika yang diklik bukan dropdown toggle, tutup dropdown
+            if (!dropdownMenu.classList.contains('hidden')) {
+                dropdownMenu.classList.add('hidden');
+            }
+        }
+    };
+</script>
 
 </body>
 </html>
